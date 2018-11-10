980784a12d281482e916ef94b4296572eeb6e920
Chintan Shah <chintans98@gmail.com>
2018-11-09 23:25:38 -0500

    Initial commit

---
+++ app/src/main/java/com/chintan/hearthstone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chintan.hearthstone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
